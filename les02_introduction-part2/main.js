ae7faf9f92b19ce571281ddb25167f4d1cb06972
Korneel Eeckhout <korneel@pienter.tech>
2021-11-21 16:13:49 +0100

    les 2

--- les02_introduction-part2/main.js
+++ les02_introduction-part2/main.js
@@ -228,7 +228,19 @@ function sum(numOne, numTwo) {
  * @param {number} num
  * @return {'odd' | 'even'}
  */
-function oddOrEven(num) {}
+function oddOrEven(num) {
+    if (num % 2 === 0) {
+        return 'even';
+    }
+
+    return 'odd';
+
+    // if (num % 2) {
+    //     return 'odd';
+    // }
+    //
+    // return 'even';
+}
 
 /**
  * Check if num is divisible by div
@@ -236,14 +248,19 @@ function oddOrEven(num) {}
  * @param {number} div
  * @return {boolean}
  */
-function isDivisible(num, div) {}
+function isDivisible(num, div) {
+    return num % div === 0;
+}
 
 /**
  * Calculate square of numOne
  * @param {number} numOne
  * @return {number}
  */
-function square(numOne) {}
+function square(numOne) {
+    return numOne * numOne;
+    // return numOne ** 2;
+}
 
 /**
  * Return the lowest number
@@ -252,7 +269,25 @@ function square(numOne) {}
  * @param {number} numThree
  * @return {number}
  */
-function min(numOne, numTwo, numThree) {}
+function min(numOne, numTwo, numThree) {
+    if (numOne < numTwo && numOne < numThree) {
+        return numOne;
+    } else if (numTwo < numThree) {
+        return numTwo;
+    } else {
+        return numThree;
+    }
+
+    //     if (numOne < numTwo && numOne < numThree) {
+    //         return numOne;
+    //     }
+
+    //     if (numTwo < numThree) {
+    //         return numTwo;
+    //     }
+    //
+    //     return numThree;
+}
 
 /**
  * Return the highest number
@@ -261,7 +296,17 @@ function min(numOne, numTwo, numThree) {}
  * @param {number} numThree
  * @return {number}
  */
-function max(numOne, numTwo, numThree) {}
+function max(numOne, numTwo, numThree) {
+    if (numOne > numTwo && numOne > numThree) {
+        return numOne;
+    }
+
+    if (numTwo > numThree) {
+        return numTwo;
+    }
+
+    return numThree;
+}
 
 /**
  * Perform the given operation on the two numbers
@@ -270,7 +315,38 @@ function max(numOne, numTwo, numThree) {}
  * @param {number} numTwo
  * @return {number}
  */
-function calculate(opp, numOne, numTwo) {}
+function calculate(opp, numOne, numTwo) {
+    if (opp === 'sum') {
+        return numOne + numTwo;
+    }
+
+    if (opp === 'divide') {
+        return numOne / numTwo;
+    }
+
+    if (opp === 'product') {
+        return numOne * numTwo;
+    }
+
+    if (opp === 'subtract') {
+        return numOne - numTwo;
+    }
+
+    return numOne;
+
+    // switch (opp) {
+    //     case 'sum':
+    //         return numOne + numTwo;
+    //     case 'subtract':
+    //         return numOne - numTwo;
+    //     case 'divide':
+    //         return numOne / numTwo;
+    //     case 'product':
+    //         return numOne * numTwo;
+    //     default:
+    //         return numOne;
+    // }
+}
 
 /**
  * Generate a random number between 0 and 10
@@ -279,4 +355,151 @@ function calculate(opp, numOne, numTwo) {}
  *    > if number is higher alert: 'Too high'
  *    > if number is lower alert: 'Too low'
  */
-function guessNumber() {}
+function guessNumber() {
+    let input = parseInt(prompt('Guess a number between 1 and 10'));
+    let randomNumber = Math.ceil(Math.random() * 10);
+
+    if (input === randomNumber) {
+        alert('You win');
+    } else if (input < randomNumber) {
+        alert('Too low');
+    } else {
+        alert('Too high');
+    }
+}
+
+// LOOP
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+
+// OEFENINGEN LOOP
+
+/**
+ * If num = 4 > 1 + 2 + 3 + 4 = 10
+ * @param {number} num
+ * @return {number}
+ */
+function sumUntil(num) {
+    let sum = 0;
+
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+console.log(sumUntil(4)); // 10
+console.log(sumUntil(6)); // 21
+
+/**
+ * Count the number of words in text
+ * @param {string} text
+ * @return {number}
+ */
+function countWords(text) {
+    let count = 1;
+
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i];
+
+        if (char === ' ') {
+            if (i !== 0 && text[i - 1] !== ' ' && i !== text.length - 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+console.log(countWords('Hier schrijf ik enkele woorden.')); // 5
+console.log(countWords(' Hier ik  ook nog    een paar.')); // 6
+
+/**
+ * Return string with only first letter of each word
+ * @param {string} text
+ * @return {string}
+ */
+function firstLetterOfEachWord(text) {
+    let letters = '';
+
+    if (text[0] !== ' ') {
+        letters += text[0];
+    }
+
+    for (let i = 1; i < text.length; i++) {
+        let previousChar = text[i - 1];
+        let curChar = text[i];
+
+        if (previousChar === ' ' && curChar !== ' ') {
+            letters += curChar;
+        }
+    }
+
+    return letters;
+}
+
+console.log(
+    firstLetterOfEachWord(
+        'Roasted melon can be made aromatic by decorating with chicory sauce.',
+    ),
+); // > 'Rmcbmabdwcs'
+console.log(
+    firstLetterOfEachWord(
+        ' Roasted   melon  can be made aromatic by decorating with chicory sauce.',
+    ),
+); // > 'Rmcbmabdwcs'
+
+/**
+ * Generate random number between 0 and 10
+ * prompt for number
+ * check if number is valid (between 0 and 10)
+ *      > if not ask for new number
+ * if number is too high
+ *      > prompt for new number (with message: 'Too high, try again')
+ * if number is too low
+ *      > prompt for new number (with message: 'Too low, try again')
+ * check if number is correct
+ *      > player wins: alert 'You win, you guessed [x] times'
+ *      > confirm 'Want to try again'
+ */
+function guessingGame() {}
+
+/**
+ * Prompt for rock, paper of scissors
+ * Check if valid input
+ *      > if not ask again
+ * Convert input to number > rock = 1, paper = 2, scissors = 3
+ * Generate random number between 1 and 3
+ * Check if tie
+ *      > confirm 'It was a tie, try again?'
+ *          > if true > start over
+ *          > if false alert score
+ * Determine winner
+ *      > player = 1 - AI = 2 > AI wins
+ *      > player = 1 - AI = 3 > Player wins
+ *      > player = 2 - AI = 1 > Player wins
+ *      > player = 2 - AI = 3 > AI wins
+ *      > player = 3 - AI = 1 > AI wins
+ *      > player = 3 - AI = 2 > Player wins
+ *          > confirm '[winner] won, try again'
+ *              > if true > start over
+ *              > if false alert score
+ */
+function rockPaperScissors() {}
