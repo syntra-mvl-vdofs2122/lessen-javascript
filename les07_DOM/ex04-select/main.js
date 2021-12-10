62bcbaa88dfc0deb724fca62bb4e4ce91a24b8b5
Korneel Eeckhout <korneel@pienter.tech>
2021-12-10 15:27:35 +0100

    les 7 - DOM

---
+++ les07_DOM/ex04-select/main.js
@@ -0,0 +1,11 @@
+function genSelect(container, options) {}
+
+let form = document.getElementById('form-one');
+let data = {
+    willy: 'Willy Vervest',
+    arthur: 'Arthur Lambrechts',
+    bram: 'Bram Dieudonne',
+    lennart: 'Lennart Verschelden',
+};
+
+genSelect(form, data);
