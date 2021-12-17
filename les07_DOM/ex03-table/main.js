1a51e84c62bdfd0b5c2e78725ff3c5c6d0798f90
Korneel Eeckhout <korneel@pienter.tech>
2021-12-17 11:56:50 +0100

    les 7 - DOM

--- les07_DOM/ex03-table/main.js
+++ les07_DOM/ex03-table/main.js
@@ -4,6 +4,34 @@
  * @param {Object[]} content
  */
 
+function genTableTwo(container, content) {
+    let html = `<table><thead><tr>`;
+
+    let headers = Object.keys(content[0]);
+
+    for (let i = 0; i < headers.length; i++) {
+        html += `<th>${headers[i]}</th>`;
+    }
+
+    html += `</tr></thead><tbody>`;
+
+    for (let j = 0; j < content.length; j++) {
+        html += `<tr>`;
+
+        let values = Object.values(content[j]);
+
+        for (let k = 0; k < values.length; k++) {
+            html += `<td>${values[k]}</td>`;
+        }
+
+        html += `</tr>`;
+    }
+
+    html += `</tbody></table>`;
+
+    container.insertAdjacentHTML('beforeend', html);
+}
+
 function genTable(container, content) {
     let newTable = document.createElement('table');
     let newThead = document.createElement('thead');
@@ -19,6 +47,25 @@ function genTable(container, content) {
 
     newThead.append(newTheadRow);
     newTable.append(newThead);
+
+    let newTbody = document.createElement('tbody');
+
+    for (let j = 0; j < content.length; j++) {
+        let newTbodyRow = document.createElement('tr');
+
+        let values = Object.values(content[j]);
+
+        for (let k = 0; k < values.length; k++) {
+            let newTd = document.createElement('td');
+            newTd.innerText = values[k];
+
+            newTbodyRow.append(newTd);
+        }
+
+        newTbody.append(newTbodyRow);
+    }
+
+    newTable.append(newTbody);
 
     container.append(newTable);
 }
@@ -63,4 +110,43 @@ let data = [
     },
 ];
 
-genTable(aContainer, data);
+genTableTwo(aContainer, data);
+
+let containerTwo = document.getElementById('container-two');
+
+let dataTwo = [
+    {
+        1: 'Test1',
+        2: 'Test2',
+        3: 'Test3',
+        4: 'Test4',
+        5: 'Test5',
+        6: 'Test6',
+    },
+    {
+        1: 'Test11',
+        2: 'Test12',
+        3: 'Test13',
+        4: 'Test14',
+        5: 'Test15',
+        6: 'Test16',
+    },
+    {
+        1: 'Test21',
+        2: 'Test22',
+        3: 'Test23',
+        4: 'Test24',
+        5: 'Test25',
+        6: 'Test26',
+    },
+];
+
+genTableTwo(containerTwo, dataTwo);
+
+// let testDiv = document.getElementById('test');
+//
+// let innerText = testDiv.innerText;
+//
+// let textContent = testDiv.textContent;
+//
+// console.log({ innerText, textContent });
--- les07_DOM/ex04-select/main.js
+++ les07_DOM/ex04-select/main.js
@@ -1,4 +1,40 @@
-function genSelect(container, options) {}
+function genSelect(container, options) {
+    let newSelect = document.createElement('select');
+
+    let keys = Object.keys(options);
+    console.log(keys);
+    for (let i = 0; i < keys.length; i++) {
+        let curKey = keys[i];
+        let curVal = options[curKey];
+
+        let newOption = document.createElement('option');
+        newOption.value = curKey;
+        newOption.innerText = curVal;
+
+        newSelect.append(newOption);
+    }
+
+    container.append(newSelect);
+}
+
+function genSelectTwo(container, options) {
+    let html = `<select>`;
+
+    let keys = Object.keys(options);
+
+    for (let i = 0; i < keys.length; i++) {
+        let curKey = keys[i];
+        let curVal = options[curKey];
+
+        html += `<option value="${curKey}">${curVal}</option>`;
+    }
+
+    html += `</select>`;
+
+    console.log(html);
+
+    container.insertAdjacentHTML('beforeend', html);
+}
 
 let form = document.getElementById('form-one');
 let data = {
@@ -8,4 +44,11 @@ let data = {
     lennart: 'Lennart Verschelden',
 };
 
-genSelect(form, data);
+let dataTwo = {
+    one: 1,
+    two: 2,
+    three: 3,
+};
+
+genSelectTwo(form, data);
+genSelectTwo(form, dataTwo);
