03c8f0e0de2947b66864e753ae8a9589a1eee650
Korneel Eeckhout <korneel@pienter.tech>
2022-01-20 13:40:01 +0100

    les 9 - rock paper scissors

--- les08_Events/ex2_rock-paper-scissors-v2/main.js
+++ les08_Events/ex2_rock-paper-scissors-v2/main.js
@@ -6,9 +6,13 @@ let $aiChoice = document.getElementById('ai-choice');
 let $messageContainer = document.getElementById('message-container');
 let $userScore = document.getElementById('user-score');
 let $aiScore = document.getElementById('ai-score');
+let $playAgainBtn = document.getElementById('play-again-btn');
+let $bestOfContainers = document.querySelectorAll('.best-of-container');
 
 let userScore = 0;
 let aiScore = 0;
+let bestOf = 2;
+let gameOver = false;
 
 let thinkingTimeout;
 let thinkingInterval;
@@ -46,14 +50,20 @@ function updateScores() {
 }
 
 function init() {
+    gameOver = false;
     clearChoiceImages();
 
     $messageContainer.innerText = '';
+
+    for (let i = 0; i < $bestOfContainers.length; i++) {
+        $bestOfContainers[i].innerText = bestOf;
+    }
 
     userScore = 0;
     aiScore = 0;
     updateScores();
 
+    $playAgainBtn.classList.add('hide');
     startThinking();
 }
 
@@ -76,10 +86,14 @@ function stopThinking() {
     clearTimeout(thinkingTimeout);
     clearInterval(thinkingInterval);
 
-    thinkingTimeout = setTimeout(startThinking, 2500);
+    thinkingTimeout = setTimeout(startThinking, 2000);
 }
 
 function choose(currentChoice) {
+    if (gameOver) {
+        return;
+    }
+
     stopThinking();
     let aiChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -88,20 +102,41 @@ function choose(currentChoice) {
     $aiChoice.classList.add(generateChoiceClass(aiChoice));
 
     if (aiChoice === currentChoice) {
-        $messageContainer.innerText = `It's a tie`;
+        $messageContainer.innerText = `This round is a tie`;
     } else if (
         (currentChoice === 1 && aiChoice === 3) ||
         (currentChoice === 2 && aiChoice === 1) ||
         (currentChoice === 3 && aiChoice === 2)
     ) {
         userScore++;
-        $messageContainer.innerText = `You won`;
+        $messageContainer.innerText = `You won this round`;
     } else {
         aiScore++;
-        $messageContainer.innerText = 'You lost';
+        $messageContainer.innerText = 'You lost this round';
     }
 
     updateScores();
+
+    if (
+        userScore > bestOf / 2 ||
+        aiScore > bestOf / 2 ||
+        userScore + aiScore === bestOf
+    ) {
+        clearTimeout(thinkingTimeout);
+        let message = 'You won this match';
+
+        if (aiScore > userScore) {
+            message = 'You lost this match';
+        }
+
+        if (aiScore === userScore) {
+            message = 'This match is a tie';
+        }
+
+        $messageContainer.innerText = message;
+        gameOver = true;
+        $playAgainBtn.classList.remove('hide');
+    }
 }
 
 function chooseRock() {
@@ -119,5 +154,6 @@ function chooseScissors() {
 $rockBtn.addEventListener('click', chooseRock);
 $paperBtn.addEventListener('click', choosePaper);
 $scissorsBtn.addEventListener('click', chooseScissors);
+$playAgainBtn.addEventListener('click', init);
 
 init();
