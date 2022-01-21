9ade3a57a059f13ab2e6f07819301c621756c6b8
Korneel Eeckhout <korneel@pienter.tech>
2022-01-21 13:48:32 +0100

    les 10 - calculator v2

--- les10-event-bubbling/Ex1-calculator-v2/main.js
+++ les10-event-bubbling/Ex1-calculator-v2/main.js
@@ -1,4 +1,5 @@
 let $inputContainer = document.querySelector('.input-container');
+let $sumAction = document.getElementById('sum-action');
 
 function test(event) {
     if (event.target.matches('.delete-btn')) {
@@ -8,3 +9,21 @@ function test(event) {
 }
 
 $inputContainer.addEventListener('click', test);
+
+function testTwo(event) {
+    let $inputFields = document.querySelectorAll('.input-container input');
+    let values = [];
+    console.log($inputFields);
+
+    // for (let i = 0; i < $inputFields.length; i++) {
+    //     values.push($input[i].value);
+    // }
+
+    $inputFields.forEach(function ($input) {
+        values.push($input.value);
+    });
+
+    console.log(values);
+}
+
+$sumAction.addEventListener('click', testTwo);
