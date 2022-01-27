e0f9dafbe79271c2a6ebcd37b94004b0fdfa5cef
Korneel Eeckhout <korneel@pienter.tech>
2022-01-27 13:58:22 +0100

    les 10 - calculator v2

--- les10-event-bubbling/Ex1-calculator-v2/main.js
+++ les10-event-bubbling/Ex1-calculator-v2/main.js
@@ -1,29 +1,116 @@
 let $inputContainer = document.querySelector('.input-container');
-let $sumAction = document.getElementById('sum-action');
+let $addActionBtn = document.getElementById('add-action');
+let $sumActionBtn = document.getElementById('sum-action');
+let $productActionBtn = document.getElementById('product-action');
+let $resultContainer = document.getElementById('result-container');
 
-function test(event) {
-    if (event.target.matches('.delete-btn')) {
-        console.log(event.target);
-        console.log(event.target.closest('.input-row'));
+let currentOperation = null;
+
+function setDeleteBtnState() {
+    let doDisable = $inputContainer.children.length <= 2;
+
+    let $deleteBtns = document.querySelectorAll('.delete-btn');
+    for (let i = 0; i < $deleteBtns.length; i++) {
+        $deleteBtns[i].disabled = doDisable;
     }
 }
 
-$inputContainer.addEventListener('click', test);
+function deleteRow(event) {
+    console.log('click');
+    let $target = event.target;
 
-function testTwo(event) {
-    let $inputFields = document.querySelectorAll('.input-container input');
-    let values = [];
-    console.log($inputFields);
-
-    // for (let i = 0; i < $inputFields.length; i++) {
-    //     values.push($input[i].value);
+    // if ($inputContainer.children.length === 2) {
+    //     return;
     // }
 
-    $inputFields.forEach(function ($input) {
-        values.push($input.value);
-    });
+    if ($target.matches('.delete-btn')) {
+        $target.closest('.input-row').remove();
+        onInput();
 
-    console.log(values);
+        setDeleteBtnState();
+    }
 }
 
-$sumAction.addEventListener('click', testTwo);
+function addRow() {
+    let rowHtml = `
+            <div class="input-row">
+                <input type="number" />
+                <button class="delete-btn">Delete</button>
+            </div>`;
+    $inputContainer.insertAdjacentHTML('beforeend', rowHtml);
+
+    setDeleteBtnState();
+
+    // let $inputRow = document.createElement('div');
+    // $inputRow.classList.add('input-row');
+    // let $inputEl = document.createElement('input');
+    // $inputEl.type = 'number';
+    // $inputRow.append($inputEl);
+    // let $delBtn = document.createElement('button');
+    // $delBtn.classList.add('delete-btn');
+    // $delBtn.innerText = 'Delete';
+    // $inputRow.append($delBtn);
+    //
+    // $inputContainer.append($inputRow);
+}
+
+function getAllInput() {
+    let inputVals = [];
+    let $inputs = document.querySelectorAll('.input-container input');
+
+    for (let i = 0; i < $inputs.length; i++) {
+        let value = parseInt($inputs[i].value);
+        if (!Number.isNaN(value)) {
+            inputVals.push(value);
+        }
+    }
+
+    return inputVals;
+}
+
+function sum() {
+    currentOperation = 'sum';
+    console.log('sum');
+    let values = getAllInput();
+    let result = 0;
+
+    for (let i = 0; i < values.length; i++) {
+        result += values[i];
+    }
+
+    console.log(result);
+    $resultContainer.innerText = 'Sum: ' + result;
+}
+
+function product() {
+    currentOperation = 'product';
+    console.log('product');
+    let values = getAllInput();
+    let result = 1;
+
+    for (let i = 0; i < values.length; i++) {
+        result *= values[i];
+    }
+    console.log(result);
+    $resultContainer.innerText = 'Product: ' + result;
+}
+
+function onInput() {
+    console.log('test');
+    switch (currentOperation) {
+        case 'sum':
+            sum();
+            break;
+        case 'product':
+            product();
+            break;
+    }
+}
+
+$inputContainer.addEventListener('click', deleteRow);
+$addActionBtn.addEventListener('click', addRow);
+$sumActionBtn.addEventListener('click', sum);
+$productActionBtn.addEventListener('click', product);
+$inputContainer.addEventListener('input', onInput);
+
+setDeleteBtnState();
