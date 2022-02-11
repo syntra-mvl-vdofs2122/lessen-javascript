7a1e9119ce847647c3b0653b274f2677efd69d29
Korneel Eeckhout <korneel@pienter.tech>
2022-02-11 13:57:43 +0100

    les 13 - array methods

--- les13-array-methods/exercises/main.js
+++ les13-array-methods/exercises/main.js
@@ -7,76 +7,35 @@
 // input: [1,2,3]
 // expected output: [2,4,6]
 
-function doubleNumbers(arr) {
-    return arr.map(function (val) {
-        return val * 2;
-    });
-}
+function doubleNumbers(arr) {}
 
 // Take an array of numbers and make them strings
 // Example:
 // input: [1,2,3]
 // expected output: ['1','2','3']
 
-function stringItUp(arr) {
-    return arr.map(function (val, i) {
-        // return val.toUpperCase();
-        return val.toString();
-        // return `${val}`;
-        // return '' + val;
-    });
-}
+function stringItUp(arr) {}
 
 // Capitalize each of an array of names
 // Example:
 // input: ['jan', 'AN', 'Ben']
 // expected output: ['Jan', 'An', 'Ben']
 
-function capitalizeNames(arr) {
-    return arr.map(function (val) {
-        return val[0].toUpperCase() + val.slice(1).toLowerCase();
-    });
-
-    // return arr.map(function (val) {
-    //   val = val.split('');
-    //   let test = val.map(function (letr, i) {
-    //     if (i === 0) {
-    //       return letr.toUpperCase();
-    //     } else {
-    //       return letr.toLowerCase();
-    //     }
-    //   });
-    //   return test.join('');
-    // });
-}
+function capitalizeNames(arr) {}
 
 // Make an array of strings of the names
 // Example:
 // input: [{name: 'Jan', age: 12}, {name: 'An', age: 33}]
 // expected output: ['Jan', 'An']
 
-function namesOnly(arr) {
-    return arr.map(function (val) {
-        return val.name;
-    });
-
-    // return arr.map((val)=> val.name);
-}
+function namesOnly(arr) {}
 
 // Make an array of strings of the names saying whether or not they can go to The Matrix (older than 18)
 // Example:
 // input: [{name: 'Jan', age: 12}, {name: 'An', age: 33}]
 // expected output: ['Jan is under age!!', 'An can go to The Matrix']
 
-function makeStrings(arr) {
-    return arr.map(function (val) {
-        if (val['age'] >= 18) {
-            return val['name'] + ' can go to The Matrix';
-        } else {
-            return `${val.name}  is under age!!`;
-        }
-    });
-}
+function makeStrings(arr) {}
 
 /*
  * ARRAY REDUCE EXERCISES
@@ -87,72 +46,35 @@ function makeStrings(arr) {
 // input: [1,2,3]
 // expected output: 6
 
-function total(arr) {
-    return arr.reduce(function (acc, val) {
-        return acc + val;
-    });
-}
+function total(arr) {}
 
 // Turn an array of numbers into a long string of all those numbers.
 // Example:
 // input: [1,2,3]
 // expected output: '123'
 
-function stringConcat(arr) {
-    return arr.reduce(function (accumulator, current) {
-        return accumulator + current;
-    }, '');
-
-    // return arr.reduce(function (acc, val) {
-    //     return acc.toString() + val.toString();
-    // });
-}
+function stringConcat(arr) {}
 
 // Turn an array of voter objects into a count of how many people voted
 // Example:
 // input: [{name: 'Jan', age: 12, voted: false}, {name: 'An', age: 33, voted: true}, {name: 'Ben', age: 21, voted: true}]
 // expected output: 2
 
-function totalVotes(arr) {
-    return arr.reduce(function (acc, val) {
-        if (val.voted) {
-            return acc + 1;
-        } else {
-            return acc;
-        }
-    }, 0);
-
-    // return arr.reduce(function (accumulator, current) {
-    //     return accumulator + current.voted;
-    // }, 0);
-}
+function totalVotes(arr) {}
 
 // Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
 // Example:
 // input: [{ title: "Snoep", price: 500 }, { title: "Brood", price: 200 },]
 // expected output: 700
 
-function shoppingSpree(arr) {
-    return arr.reduce(function (acc, val) {
-        return acc + val.price;
-    }, 0);
-}
+function shoppingSpree(arr) {}
 
 // Given an array of arrays, flatten them into a single array
 // Example:
 // input: [[1,2,3],[6,5,4]]
 // expected output: [1,2,3,6,5,4]
 
-function flatten(arr) {
-    // let arrOne = [1, 2, 3];
-    // let arrTwo = [6, 7, 8];
-    //
-    // console.log(arrOne.concat(arrTwo));
-
-    return arr.reduce(function (acc, val) {
-        return acc.concat(val);
-    }, []);
-}
+function flatten(arr) {}
 
 /*
  * ARRAY FILTER EXERCISES
@@ -163,53 +85,32 @@ function flatten(arr) {
 // input: [1,2,3,6,5,4]
 // expected output: [6,5]
 
-function fiveAndGreaterOnly(arr) {
-    return arr.filter(function (age) {
-        return age >= 5;
-    });
-}
+function fiveAndGreaterOnly(arr) {}
 
 // Given an array of numbers, return a new array that only includes the even numbers.
 // Example:
 // input: [1,2,3,6,5,4]
 // expected output: [2,6,4]
 
-function evensOnly(arr) {
-    return arr.filter(function (val) {
-        return val % 2 === 0;
-        // return !(val % 2)
-    });
-}
+function evensOnly(arr) {}
 
 // Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
 // Example:
 // input: ['lange string', 'kort']
 // expected output: ['kort']
 
-function fiveCharactersOrFewerOnly(arr) {
-    return arr.filter(function (val) {
-        return val.length <= 5;
-    });
-}
+function fiveCharactersOrFewerOnly(arr) {}
 
 // Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
 // Example:
 // input: [{name: 'Jan', age: 12, member: false}, {name: 'An', age: 33, member: true}, {name: 'Ben', age: 21, member: true}]
-// expected output: [{name: 'Jan', age: 12, member: false}, {name: 'Ben', age: 21, member: true}]
+// expected output: [{name: 'An', age: 33, member: true}, {name: 'Ben', age: 21, member: true}]
 
-function peopleWhoBelongToTheIlluminati(arr) {
-    return arr.filter(function (val) {
-        return val.member;
-    });
-}
+function peopleWhoBelongToTheIlluminati(arr) {}
 
 // Make a filtered list of all the people who are old enough to see The Matrix (older than 18)
 // Example:
 // input: [{name: 'Jan', age: 12}, {name: 'An', age: 33}]
 // expected output: [{name: 'An', age: 33}]
 
-function ofAge(arr) {
-    return arr.filter(function (val) {
-        return val.age > 18;
-    });
-}
+function ofAge(arr) {}
---
+++ les13-array-methods/main.js
@@ -0,0 +1,141 @@
+// Array methods
+
+let anArray = [1, 2, 3, 4, 5];
+
+// forEach
+Array.prototype.myForEach = function (cbFunction) {
+    for (let i = 0; i < this.length; i++) {
+        cbFunction(this[i], i, this);
+    }
+};
+
+function logItem(element, index, array) {
+    console.log({ element, index, array });
+    // console.log({
+    //     element: element,
+    //     index: index,
+    //     array: array,
+    // });
+}
+
+anArray.forEach(logItem);
+
+anArray.myForEach(logItem);
+
+function doubleItem(element) {
+    console.log(element * 2);
+}
+
+anArray.forEach(doubleItem);
+anArray.myForEach(doubleItem);
+
+// filter
+
+Array.prototype.myFilter = function (cbFunction) {
+    let filteredArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let cbReturn = cbFunction(this[i], i, this);
+        console.log({ cbReturn, element: this[i] });
+
+        if (cbReturn) {
+            filteredArray.push(this[i]);
+        }
+    }
+
+    return filteredArray;
+};
+
+function onlyEven(element, index, array) {
+    return element % 2 === 0;
+}
+
+function onlyOdd(element, index, array) {
+    return element % 2 > 0;
+}
+
+console.log(anArray.filter(onlyEven));
+console.log(anArray.myFilter(onlyEven));
+console.log(anArray.myFilter(onlyOdd));
+
+// map
+
+Array.prototype.myMap = function (cbFunction) {
+    let mappedArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let cbReturn = cbFunction(this[i], i, this);
+
+        mappedArray.push(cbReturn);
+    }
+
+    return mappedArray;
+};
+
+function returnDoubleItem(element, index, array) {
+    return element * 2;
+}
+
+console.log(anArray.map(returnDoubleItem));
+
+let peopleArray = [
+    {
+        firstName: 'Lennart',
+        lastName: 'Verschelden',
+        age: 15,
+    },
+    {
+        firstName: 'Bram',
+        lastName: 'Dieudonne',
+        age: 2,
+    },
+    {
+        firstName: 'Ernest',
+        lastName: 'Debruyne',
+        age: 8,
+    },
+];
+
+function fullName(element, index, array) {
+    return element.firstName + ' ' + element.lastName;
+}
+
+console.log(peopleArray.map(fullName));
+console.log(peopleArray.myMap(fullName));
+
+// reduce
+
+Array.prototype.myReduce = function (cbFunction, initialVal) {
+    let reduceResult = initialVal;
+    let startLoop = 0;
+
+    if (initialVal === undefined) {
+        reduceResult = this[0];
+        startLoop = 1;
+    }
+
+    for (let i = startLoop; i < this.length; i++) {
+        reduceResult = cbFunction(reduceResult, this[i], i, this);
+    }
+
+    return reduceResult;
+};
+
+function sum(prevVal, element, index, array) {
+    console.log({ prevVal, element });
+    return prevVal + element;
+}
+
+console.log(anArray.reduce(sum));
+console.log(anArray.myReduce(sum));
+
+function countOverSix(prevVal, element, index, array) {
+    if (element.age > 6) {
+        prevVal++;
+    }
+
+    return prevVal;
+}
+
+console.log(peopleArray.reduce(countOverSix, 0));
+console.log(peopleArray.myReduce(countOverSix, 0));
